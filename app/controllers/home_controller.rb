ccc0cc0df46c62c508bbee0451a5cb407689502f
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2006-09-22 15:43:07 +0000

    PlanetaShop: feed icon added to home
    git-svn-id: svn+ssh://69.61.74.106/home/repos/planetashop/trunk@242 cca48cc7-e311-0410-b82d-d4683d06456e

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   caches_page :index, :rss
 
   def index
-    mercado  = Country.find_by_name(params[:country] || 'argentina')
+    mercado  = Country.find_by_name(params[:country])
     @pages, @items = paginate_collection mercado.items, :per_page => 8, :page => (params[:page] || 1)
     @distros = Story.find(:all)
   end
